527603d739c7c3210a5a63546d7aed18adf39178
T-Stewart <70151951+T-Stewart@users.noreply.github.com>
2021-02-07 14:09:37 +0000

    refactored Deck and Game components

--- src/Components/Cards/Deck.js
+++ src/Components/Cards/Deck.js
@@ -1,7 +1,324 @@
 import Cards from './Cards' 
- const Deck = [
-    
-         {
+//  const Pack = {
+      
+//       twoOfClubs: {
+//             id: '2 of Clubs' ,
+//             image: Cards.clubs2,
+//             score: 2,
+//             suit: 'clubs'
+//             },
+//       threeOfClubs:   {
+//             id: '3 of Clubs' ,
+//             image: Cards.clubs3,
+//             score: 3,
+//             suit: 'clubs'
+//             },
+//       fourOfClubs:   {
+//             id: '4 of Clubs' ,
+//             image: Cards.clubs4 ,
+//             score: 4,
+//             suit: 'clubs'
+//             },
+//       fiveOfClubs:   {
+//             id: '5 of Clubs' ,
+//             image: Cards.clubs5,
+//             score: 5,
+//             suit: 'clubs'
+//             },
+//       sixOfClubs:   {
+//             id: '6 of Clubs' ,
+//             image: Cards.clubs6 ,
+//             score: 6,
+//             suit: 'clubs'
+//             },
+//       sevenOfClubs:   {
+//             id: '7 of Clubs' ,
+//             image: Cards.clubs7 ,
+//             score: 7,
+//             suit: 'clubs'
+//             },
+//       eightOfClubs:   {
+//             id: '8 of Clubs' ,
+//             image: Cards.clubs8 ,
+//             score: 8,
+//             suit: 'clubs'
+//             },
+//       nineOfClubs:  {
+//             id: '9 of Clubs' ,
+//             image: Cards.clubs9,
+//             score: 9,
+//             suit: 'clubs'
+//             },
+//       tenOfClubs:   {
+//             id: '10 of Clubs' ,
+//             image: Cards.clubs10,
+//             score: 10,
+//             suit: 'clubs'
+//             },
+//       jackOfClubs:  {
+//             id: 'J of Clubs' ,
+//             image: Cards.clubsJ,
+//             score: 10,
+//             suit: 'clubs'
+//             },
+//       queenOfClubs:  {
+//             id: 'Q of Clubs' ,
+//             image: Cards.clubsQ,
+//             score: 10,
+//             suit: 'clubs'
+//             },
+//       kingOfClubs:   {
+//             id: 'K of Clubs' ,
+//             image: Cards.clubsK,
+//             score: 10,
+//             suit: 'clubs'
+//             },
+//       aceOfClubs:   {
+//              id: 'A of Clubs' ,
+//             image: Cards.clubsA,
+//             score: 10,
+//             suit: 'clubs'
+//             },
+
+//       twoOfSpades:  {
+//             id: '2 of Spades' ,
+//             image: Cards.spades2 ,
+//             score: 2,
+//             suit: 'spades'
+//             },
+//       threeOfSpades:  {
+//             id: '3 of Spades' ,
+//             image: Cards.spades3,
+//             score: 3,
+//             suit: 'spades'
+//             },
+//       fourOfSpades:     {
+//             id: '4 of Spades' ,
+//             image: Cards.spades4,
+//             score: 4,
+//             suit: 'spades'
+//             },
+//       fiveOfSpades:  {
+//             id: '5 of Spades' ,
+//             image: Cards.spades5,
+//             score: 5,
+//             suit: 'spades'
+//             },
+//       sixOfSpades:  {
+//             id: '6 of Spades' ,
+//             image: Cards.spades6,
+//             score: 6,
+//             suit: 'spades'
+//             },
+//       sevenOfSpades:  {
+//             id: '7 of Spades' ,
+//             image: Cards.spades7,
+//             score: 7,
+//             suit: 'spades'
+//             },
+//       eightOfSpades:  {
+//             id: '8 of Spades' ,
+//             image: Cards.spades8,
+//             score: 8,
+//             suit: 'spades'
+//             },
+//       nineOfSpades:  {
+//             id: '9 of Spades' ,
+//             image: Cards.spades9,
+//             score: 9,
+//             suit: 'spades'
+//             },
+//       tenOfSpades:  {
+//             id: '10 of Spades' ,
+//             image: Cards.spades10,
+//             score: 10,
+//             suit: 'spades'
+//             },
+//       jackOfSpades:  {
+//             id: 'J of Spades' ,
+//             image: Cards.spadesJ,
+//             score: 10,
+//             suit: 'spades'
+//             },
+//       queenOfSpades:  {
+//             id: 'Q of Spades' ,
+//             image: Cards.spadesQ,
+//             score: 10,
+//             suit: 'spades'
+//             },
+//       kingOfSpades:  {
+//             id: 'K of Spades' ,
+//             image: Cards.spadesK,
+//             score: 10,
+//             suit: 'spades'
+//             },
+//       aceOfSpades:  {
+//              id: 'A of Spades' ,
+//             image: Cards.spadesA,
+//             score: 10,
+//             suit: 'spades'
+//             },
+
+//       twoOfHearts:  {
+//             id: '2 of Hearts' ,
+//             image: Cards.hearts2,
+//             score: 2,
+//             suit: 'hearts'
+//             },
+//       threeOfHearts:   {
+//             id: '3 of Hearts' ,
+//             image: Cards.hearts3,
+//             score: 3,
+//             suit: 'hearts'
+//             },
+//       fourOfHearts:   {
+//             id: '4 of Hearts' ,
+//             image: Cards.hearts4,
+//             score: 4,
+//             suit: 'hearts'
+//             },
+//       fiveOfHearts:   {
+//             id: '5 of Hearts' ,
+//             image: Cards.hearts5,
+//             score: 5,
+//             suit: 'hearts'
+//             },
+//       sixOfHearts:{
+//             id: '6 of Hearts' ,
+//             image: Cards.hearts6,
+//             score: 6,
+//             suit: 'hearts'
+//             },
+//       sevenOfHearts:   {
+//             id: '7 of Hearts' ,
+//             image: Cards.hearts7,
+//             score: 7,
+//             suit: 'hearts'
+//             },
+//       eightOfHearts:   {
+//             id: '8 of Hearts' ,
+//             image: Cards.hearts8,
+//             score: 8,
+//             suit: 'hearts'
+//             },
+//       nineOfHearts:  {
+//             id: '9 of Hearts' ,
+//             image: Cards.hearts9,
+//             score: 9,
+//             suit: 'hearts'
+//             },
+//       tenOfHearts:   {
+//             id: '10 of Hearts' ,
+//             image: Cards.hearts10,
+//             score: 10,
+//             suit: 'hearts'
+//             },
+//       jackOfHearts:   {
+//             id: 'J of Hearts' ,
+//             image: Cards.heartsJ,
+//             score: 10,
+//             suit: 'hearts'
+//             },
+//       queenOfHearts:   {
+//             id: 'Q of Hearts' ,
+//             image: Cards.heartsQ,
+//             score: 10,
+//             suit: 'hearts'
+//             },
+//       kingOfHearts:   {
+//             id: 'K of Hearts' ,
+//             image: Cards.heartsK,
+//             score: 10,
+//             suit: 'hearts'
+//             },
+//       AceOfHearts:   {
+//              id: 'A of Hearts' ,
+//             image: Cards.heartsA,
+//             score: 10,
+//             suit: 'hearts'
+//             },
+//       twoOfDiamonds:               {
+//             id: '2 of Diamonds' ,
+//             image: Cards.diamonds2,
+//             score: 2,
+//             suit: 'diamonds'
+//             },
+//       threeOfDiamonds:   {
+//             id: '3 of Diamonds' ,
+//             image: Cards.diamonds3,
+//             score: 3,
+//             suit: 'diamonds'
+//             },
+//       fourOfDiamonds:   {
+//             id: '4 of Diamonds' ,
+//             image: Cards.diamonds4,
+//             score: 4,
+//             suit: 'diamonds'
+//             },
+//       fiveOfDiamonds:   {
+//             id: '5 of Diamonds' ,
+//             image: Cards.diamonds5,
+//             score: 5,
+//             suit: 'diamonds'
+//             },
+//       sixOfDiamonds:   {
+//             id: '6 of Diamonds' ,
+//             image: Cards.diamonds6,
+//             score: 6,
+//             suit: 'diamonds'
+//             },
+//       sevenOfDiamonds:   {
+//             id: '7 of Diamonds' ,
+//             image: Cards.diamonds7,
+//             score: 7,
+//             suit: 'diamonds'
+//             },
+//       eightOfDiamonds:   {
+//             id: '8 of Diamonds' ,
+//             image: Cards.diamonds8,
+//             score: 8,
+//             suit: 'diamonds'
+//             },
+//       nineOfDiamonds:  {
+//             id: '9 of Diamonds' ,
+//             image: Cards.diamonds9,
+//             score: 9,
+//             suit: 'diamonds'
+//             },
+//       tenOfDiamonds:   {
+//             id: '10 of Diamonds' ,
+//             image: Cards.diamonds10,
+//             score: 10,
+//             suit: 'diamonds'
+//             },
+//       jackOfDiamonds:   {
+//             id: 'J of Diamonds' ,
+//             image: Cards.diamondsJ,
+//             score: 10,
+//             suit: 'diamonds'
+//             },
+//       queenOfDiamonds:   {
+//             id: 'Q of Diamonds' ,
+//             image: Cards.diamondsQ,
+//             score: 10,
+//             suit: 'diamonds'
+//             },
+//       kingOfDiamonds:   {
+//             id: 'K of Diamonds' ,
+//             image: Cards.diamondsK,
+//             score: 10,
+//             suit: 'diamonds'
+//             },
+//       aceOfDiamonds:   {
+//              id: 'A of Diamonds' ,
+//             image: Cards.diamondsA,
+//             score: 10,
+//             suit: 'diamonds'
+//             },
+//          },
+const Sack = [
+      
+      {
             id: '2 of Clubs' ,
             image: Cards.clubs2,
             score: 2,
@@ -25,43 +342,43 @@ import Cards from './Cards'
             score: 5,
             suit: 'clubs'
             },
-         {
+        {
             id: '6 of Clubs' ,
             image: Cards.clubs6 ,
             score: 6,
             suit: 'clubs'
             },
-         {
+      {
             id: '7 of Clubs' ,
             image: Cards.clubs7 ,
             score: 7,
             suit: 'clubs'
             },
-         {
+       {
             id: '8 of Clubs' ,
             image: Cards.clubs8 ,
             score: 8,
             suit: 'clubs'
             },
-        {
+{
             id: '9 of Clubs' ,
             image: Cards.clubs9,
             score: 9,
             suit: 'clubs'
             },
-         {
+{
             id: '10 of Clubs' ,
             image: Cards.clubs10,
             score: 10,
             suit: 'clubs'
             },
-         {
+{
             id: 'J of Clubs' ,
             image: Cards.clubsJ,
             score: 10,
             suit: 'clubs'
             },
-         {
+        {
             id: 'Q of Clubs' ,
             image: Cards.clubsQ,
             score: 10,
@@ -73,105 +390,105 @@ import Cards from './Cards'
             score: 10,
             suit: 'clubs'
             },
-         {
+        {
              id: 'A of Clubs' ,
             image: Cards.clubsA,
             score: 10,
             suit: 'clubs'
             },
 
-          {
+       {
             id: '2 of Spades' ,
             image: Cards.spades2 ,
             score: 2,
             suit: 'spades'
             },
-         {
+        {
             id: '3 of Spades' ,
             image: Cards.spades3,
             score: 3,
             suit: 'spades'
             },
-         {
+           {
             id: '4 of Spades' ,
             image: Cards.spades4,
             score: 4,
             suit: 'spades'
             },
-         {
+        {
             id: '5 of Spades' ,
             image: Cards.spades5,
             score: 5,
             suit: 'spades'
             },
-         {
+       {
             id: '6 of Spades' ,
             image: Cards.spades6,
             score: 6,
             suit: 'spades'
             },
-         {
+        {
             id: '7 of Spades' ,
             image: Cards.spades7,
             score: 7,
             suit: 'spades'
             },
-         {
+        {
             id: '8 of Spades' ,
             image: Cards.spades8,
             score: 8,
             suit: 'spades'
             },
-        {
+       {
             id: '9 of Spades' ,
             image: Cards.spades9,
             score: 9,
             suit: 'spades'
             },
-         {
+        {
             id: '10 of Spades' ,
             image: Cards.spades10,
             score: 10,
             suit: 'spades'
             },
-         {
+        {
             id: 'J of Spades' ,
             image: Cards.spadesJ,
             score: 10,
             suit: 'spades'
             },
-         {
+        {
             id: 'Q of Spades' ,
             image: Cards.spadesQ,
             score: 10,
             suit: 'spades'
             },
-         {
+        {
             id: 'K of Spades' ,
             image: Cards.spadesK,
             score: 10,
             suit: 'spades'
             },
-         {
+        {
              id: 'A of Spades' ,
             image: Cards.spadesA,
             score: 10,
             suit: 'spades'
             },
 
-          {
+        {
             id: '2 of Hearts' ,
             image: Cards.hearts2,
             score: 2,
             suit: 'hearts'
             },
-         {
+       {
             id: '3 of Hearts' ,
             image: Cards.hearts3,
             score: 3,
             suit: 'hearts'
             },
-         {
+        {
             id: '4 of Hearts' ,
             image: Cards.hearts4,
             score: 4,
@@ -183,13 +500,13 @@ import Cards from './Cards'
             score: 5,
             suit: 'hearts'
             },
-         {
+      {
             id: '6 of Hearts' ,
             image: Cards.hearts6,
             score: 6,
             suit: 'hearts'
             },
-         {
+        {
             id: '7 of Hearts' ,
             image: Cards.hearts7,
             score: 7,
@@ -216,7 +533,8 @@ import Cards from './Cards'
          {
             id: 'J of Hearts' ,
             image: Cards.heartsJ,
-            score: 10
+            score: 10,
+            suit: 'hearts'
             },
          {
             id: 'Q of Hearts' ,
@@ -224,13 +542,13 @@ import Cards from './Cards'
             score: 10,
             suit: 'hearts'
             },
-         {
+        {
             id: 'K of Hearts' ,
             image: Cards.heartsK,
             score: 10,
             suit: 'hearts'
             },
-         {
+        {
              id: 'A of Hearts' ,
             image: Cards.heartsA,
             score: 10,
@@ -242,7 +560,7 @@ import Cards from './Cards'
             score: 2,
             suit: 'diamonds'
             },
-         {
+        {
             id: '3 of Diamonds' ,
             image: Cards.diamonds3,
             score: 3,
@@ -254,19 +572,19 @@ import Cards from './Cards'
             score: 4,
             suit: 'diamonds'
             },
-         {
+        {
             id: '5 of Diamonds' ,
             image: Cards.diamonds5,
             score: 5,
             suit: 'diamonds'
             },
-         {
+       {
             id: '6 of Diamonds' ,
             image: Cards.diamonds6,
             score: 6,
             suit: 'diamonds'
             },
-         {
+        {
             id: '7 of Diamonds' ,
             image: Cards.diamonds7,
             score: 7,
@@ -283,37 +601,47 @@ import Cards from './Cards'
             image: Cards.diamonds9,
             score: 9,
             suit: 'diamonds'
-            },
-         {
+            },  
+            {
             id: '10 of Diamonds' ,
             image: Cards.diamonds10,
             score: 10,
             suit: 'diamonds'
             },
-         {
+        {
             id: 'J of Diamonds' ,
             image: Cards.diamondsJ,
             score: 10,
             suit: 'diamonds'
             },
-         {
+       {
             id: 'Q of Diamonds' ,
             image: Cards.diamondsQ,
             score: 10,
             suit: 'diamonds'
             },
-         {
+       {
             id: 'K of Diamonds' ,
             image: Cards.diamondsK,
             score: 10,
             suit: 'diamonds'
             },
-         {
-             id: 'A of Diamonds' ,
+
+            {
+            id: 'A of Diamonds' ,
             image: Cards.diamondsA,
             score: 10,
             suit: 'diamonds'
             },
-        ]
+      ],
 
-        export default Deck
+Deck =[]
+
+for( let i = 0; i < 52; i++ ){
+            let randomIndex = Sack[Math.floor(Math.random()* 52)]; 
+           
+            Deck.push(randomIndex)
+           
+      }; 
+
+export default Deck
--- src/Components/Game/Game.jsx
+++ src/Components/Game/Game.jsx
@@ -7,25 +7,16 @@ import '../Drag/Board/Board.css'
 import Board from '../Drag/Board/Board'
 import Card from '../Drag/Card/Card'
 
-
-
-
 export default class Game extends React.Component {
     constructor(props) {
         super(props)
         this.state = {  
-        randomDeck: [],
         index: 0,
-        currentScore: [],
-        addScore: [],
-        suit: [],
-        cardId: [],
         wastePile: false,
         newCard: false,
         cardPosition: { position: 'relative'},
-        dealCardPlacement: ['#card-spot1','#card-spot2','#card-spot3','#card-spot4','#card-spot5','#card-spot6','#card-spot7','#card-spot8','#card-spot9','#card-spot10'],
-        cardPlacement: ['#card-spot11','#card-spot12','#card-spot13','#card-spot14','#card-spot15','#card-spot16','#card-spot17','#card-spot18','#card-spot19','#card-spot20','#card-spot21','#card-spot22','#card-spot23','#card-spot24','#card-spot25','#card-spot26','#card-spot27','#card-spot28','#card-spot29','#card-spot30','#card-spot31','#card-spot32','#card-spot33','#card-spot34','#card-spot35','#card-spot36','#card-spot37','#card-spot38','#card-spot39','#card-spot40','#card-spot41','#card-spot42','#card-spot43','#card-spot44','#card-spot45','#card-spot46','#card-spot47','#card-spot48','#card-spot49','#card-spot50','#card-spot51','#card-spot52'],
-        randomNumber: []
+        deal: false,
+        score: 0
       }  
    };
 
@@ -51,78 +42,82 @@ export default class Game extends React.Component {
             this.setState({
                index: this.state.index + 1
             });
-         };
-         
+         };    
    };
 
-   
+   //    addScore = () => {
+   //    this.newCard()
+
+   //        this.setState({
+   //           currentScore: this.state.currentScore+ this.state.addScore[this.state.index+1]
+   //        }) 
+   // }
+
+   score = () => {
+      let scoreStart = [];
+       for( let i=0; i<10; i++){
+          scoreStart.push(Deck[i].score) 
+         };
+         if(this.state.deal === true){
+            return(
+               <div>
+                  <h1 className="score">
+                     {scoreStart.reduce((a,b) => a+b, 0)}
+                  </h1>
+               </div>
+            );
+         };
+   };
 
    displayCard = () => {
-      const deckRest = this.state.randomDeck.slice(10,52);
+      let newCards = Deck.slice(10,52)
+
         
-         if(this.state.newCard === true){
+         newCards.map(({suit, image, score}, index) => {
+         if (this.state.newCard === true) {   
+            console.log("Hello")             
             return(
-               <Card className="draggable"  draggable>
-                   <img 
-                     src={deckRest[this.state.index]}  
-                     alt="" 
-                     className="cards" 
-                     id={this.state.cardPlacement[this.state.index]}/>
-               </Card> 
-               );
-         };         
-   };
-
-   addScore = () => {
-      this.newCard()
-
-          this.setState({
-             currentScore: this.state.currentScore+ this.state.addScore[this.state.index+1]
-          }) 
-   }
-
-
+               <Card className="draggable" draggable>
+                  <img  className="cards" key={suit} src={image} value={score} id={`card-spot${index}`}alt=""/>
+               </Card>
+                           
+               )
+         }
+      })
          
-   randomDeck = () => {  
-      const tenDeck = [];
-      const scoreStart = [];
-      const suitStart = [];
-      const idStart = [];
 
-         for( let i = 0; i < 52; i++ ){
-            let randomIndex = Deck[Math.floor(Math.random()* 52)];  ;
-            tenDeck.push([randomIndex.image]);
-            scoreStart.push(randomIndex.score);
-            suitStart.push(randomIndex.suit);
-            idStart.push(randomIndex.id)
-      }; 
       
-      const add = (a, b) => a + b
-      this.setState({currentScore: scoreStart.slice(0,10).reduce(add)})
-      this.setState({addScore: scoreStart.slice(10,52)})
-      this.setState({randomDeck: tenDeck}) 
-      this.setState({suit: suitStart})   
-      this.setState({cardId: idStart})
-    };
+   }
+      
+         
+      
+      
+
+
+   deal = () => {
+     this.setState({deal: true})
+   }; 
 
     render() {
-       
+   
         return (
             <div className="card-table">
                  <div className="container" id="top-container">
                     {/* <button onClick={this.addScore}>ADDSCORE</button> */}
                      <Board className="board" >
-                    <div id="new-card" >{this.displayCard()} </div>
+                    <div id="new-card" >
+                       {this.displayCard()}
+                    </div>
                       </Board>
                     <div id="pick-up">
-                        <img src={cardBack} onClick={this.addScore} alt=""/>
+                        <img src={cardBack} onClick={this.newCard} alt=""/>
                     </div>
-                     <img onClick={this.randomDeck} src={Deal} alt=""/> 
+                     <img onClick={this.deal} src={Deal} alt=""/> 
                      <Board className="board"   >
                         <div id="waste-pile" onDragEnter={this.dropEvent} ></div>   
                     </Board>
                     <div >
-                        <h1 className="score">{this.state.currentScore}</h1>
+                       {this.score()}
                         </div>
                     </div>
                     <div className="board">
@@ -131,40 +126,22 @@ export default class Game extends React.Component {
                         <Board className="board" id="put-down"></Board>
                 </div>
                 <Board className="board" id="middle-container">
-                   <Card className="draggable"  draggable  >
-                      <img className="cards" id="card-spot1" src={this.state.randomDeck[0]} alt=""/>
-                   </Card>
-                   <Card className="draggable" draggable >
-                      <img className="cards" id="card-spot2"  src={this.state.randomDeck[1]} alt=""/>
-                   </Card>
-                   <Card className="draggable"  draggable >
-                      <img className="cards" id="card-spot3"  src={this.state.randomDeck[2]} alt=""/>
-                   </Card>
-                   <Card className="draggable"  draggable >
-                      <img className="cards" id="card-spot4"  src={this.state.randomDeck[3]} alt=""/>
-                   </Card>
-                   <Card className="draggable"  draggable >
-                      <img className="cards" id="card-spot5"  src={this.state.randomDeck[4]} alt=""/>
-                   </Card>
-                   <Card className="draggable"  draggable >
-                      <img className="cards" id="card-spot6"  src={this.state.randomDeck[5]} alt=""/>
-                   </Card>
-                   <Card className="draggable"   draggable >
-                      <img className="cards" id="card-spot7"  src={this.state.randomDeck[6]} alt=""/>
-                   </Card>
-                   <Card className="draggable"  draggable >
-                      <img className="cards" id="card-spot8" src={this.state.randomDeck[7]} alt=""/>
-                   </Card>
-                   <Card className="draggable"  draggable >
-                      <img className="cards" id="card-spot9"  src={this.state.randomDeck[8]} alt=""/>
-                   </Card>
-                   <Card className="draggable"  draggable >
-                      <img className="cards" id="card-spot10"  src={this.state.randomDeck[9]} alt=""/>
-                   </Card>
+                   {
+                      Deck.slice(0,10).map(({suit, image, score}, index) => {
+                         if (this.state.deal === true) {
+                            
+                         return(
+                            <Board className="board" id="card-container">
+                            <Card className="draggable" draggable>
+                               <img  className="cards" key={suit} src={image} value={score} id={`card-spot${index}`}alt=""/>
+                            </Card>
+                            </Board>
+                         )
+                         }
+                      })
+                   }
                 </Board>   
         </div>
         )
     }
-
-    
 }
